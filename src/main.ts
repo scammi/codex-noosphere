66858ca4ba9c218e4240c98fad28ff7bfe7927c0
scammi <scammi@gmail.com>
2025-07-04 11:39:24 -0300

    long live diego !

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 3000;
 
   app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
 
   await app.listen(port);
   console.log(`🚀 Codex Noosphere API running on port ${port}`);
